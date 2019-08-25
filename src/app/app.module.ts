151a4879823bacf0b563e78c854649c43733ea69
Gopika Kabra <gopika.kabra@gmail.com>
2019-08-25 11:07:53 +0530

    [master]: layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {MfSidebarModule} from 'mf-sidebar';
+import {MfWorkspaceModule} from 'mf-workspace';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MfSidebarModule, MfWorkspaceModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
